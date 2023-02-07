38f46c69d18346127110b6c0799f05e300aa24a9
Swapnil Jadhav <89910173+Swapnil-2503@users.noreply.github.com>
2023-02-07 16:27:00 +0530

    Add files via upload

---
+++ precache-manifest.4689ab27a5cd601294cb6d57bedf123a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "06094fc8c355bee81574",
+    "url": "./static/css/main.4281b942.chunk.css"
+  },
+  {
+    "revision": "06094fc8c355bee81574",
+    "url": "./static/js/main.06094fc8.chunk.js"
+  },
+  {
+    "revision": "920c509d8cf46ff43416",
+    "url": "./static/js/1.920c509d.chunk.js"
+  },
+  {
+    "revision": "fee337112dfd6d3fe882",
+    "url": "./static/css/2.79735033.chunk.css"
+  },
+  {
+    "revision": "fee337112dfd6d3fe882",
+    "url": "./static/js/2.fee33711.chunk.js"
+  },
+  {
+    "revision": "4d381ea8ef1c6cd20131",
+    "url": "./static/js/runtime~main.4d381ea8.js"
+  },
+  {
+    "revision": "0ff4fa7816397aed6445951ad4585af5",
+    "url": "./static/media/proj2.0ff4fa78.jpg"
+  },
+  {
+    "revision": "fc8028c73c6eb2ba98cc5689d7e07183",
+    "url": "./static/media/proj3.fc8028c7.webp"
+  },
+  {
+    "revision": "8e03f996028d702e9874d3fc41bc6c47",
+    "url": "./static/media/proj4.8e03f996.webp"
+  },
+  {
+    "revision": "699fb3e0b2d401e9786bd70dde2572cd",
+    "url": "./static/media/proj5.699fb3e0.jpg"
+  },
+  {
+    "revision": "8aea9a7c9245a37207b7bcb3bb516472",
+    "url": "./static/media/proj6.8aea9a7c.png"
+  },
+  {
+    "revision": "943880191c52126472ed20a737d168cb",
+    "url": "./index.html"
+  }
+];
