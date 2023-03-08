44fe0a1ec99e076c0a7f25e8ee3fd45750b35262
Swapnil Jadhav <89910173+Swapnil-2503@users.noreply.github.com>
2023-03-08 17:06:28 +0530

    Add files via upload

---
+++ precache-manifest.e1e405074841f3ae51dac70634f29090.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c4d6f500304da5959d4",
+    "url": "./static/css/main.5ca40eed.chunk.css"
+  },
+  {
+    "revision": "3c4d6f500304da5959d4",
+    "url": "./static/js/main.3c4d6f50.chunk.js"
+  },
+  {
+    "revision": "f6fe504b2992c8695bc4",
+    "url": "./static/js/1.f6fe504b.chunk.js"
+  },
+  {
+    "revision": "3b836718595b0aae805e",
+    "url": "./static/css/2.4876e0e8.chunk.css"
+  },
+  {
+    "revision": "3b836718595b0aae805e",
+    "url": "./static/js/2.3b836718.chunk.js"
+  },
+  {
+    "revision": "7e5ec5be59e0e0155e31",
+    "url": "./static/js/runtime~main.7e5ec5be.js"
+  },
+  {
+    "revision": "ee1093a6c9f4532e7f68f681de9ceb6b",
+    "url": "./static/media/swapnil2.ee1093a6.jpg"
+  },
+  {
+    "revision": "0ff4fa7816397aed6445951ad4585af5",
+    "url": "./static/media/proj2.0ff4fa78.jpg"
+  },
+  {
+    "revision": "cea7f76de17b9927870ce22fda98c2af",
+    "url": "./static/media/proj3.cea7f76d.jpg"
+  },
+  {
+    "revision": "862d7800c91d7f72a7ed36a3e4f52e45",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4689ab27a5cd601294cb6d57bedf123a.js"
+  "./precache-manifest.e1e405074841f3ae51dac70634f29090.js"
 );
 
 workbox.clientsClaim();
